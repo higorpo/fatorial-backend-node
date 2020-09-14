747a36ae8b0d0edd68b61cad8bbd74af4c2e69ce
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-09-14 13:39:24 -0300

    :hammer: Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(routes);
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 8000}!`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}!`);
 })
